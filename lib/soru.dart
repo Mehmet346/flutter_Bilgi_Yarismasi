9943d94b68c0d880d60f13c3621aed21bb405698
unknown <ozdogan.7547@gmail.com>
2022-12-07 20:52:34 +0300

    Delete soru.dart

--- lib/soru.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'test_veri.dart';
-
-class Soru {
-  String soruMetni;
-  bool soruYaniti;
-  Soru({required this.soruMetni, required this.soruYaniti});
-}
