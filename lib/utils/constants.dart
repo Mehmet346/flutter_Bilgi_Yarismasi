cb4c5bc2ad57720b056926dd366ee0854bfd7278
unknown <ozdogan.7547@gmail.com>
2022-12-07 21:01:46 +0300

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Icon dogruYanit = Icon(Icons.check_circle, color: Colors.green);
+const Icon yanlisYanit = Icon(Icons.cancel, color: Colors.red);
+
+var dogruYanitCount = 0;
+var yanlisYanitCount = 0;
